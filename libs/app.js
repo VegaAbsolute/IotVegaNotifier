dca05c74ceb77f88b8980098ef741085f3a463e4
Gerusenko <a.gerusenko@vega-absolute.ru>
2018-05-22 10:46:52 +0700

    test

--- libs/app.js
+++ libs/app.js
@@ -212,9 +212,9 @@ function rx(obj)
             if(config.debugMOD) console.log('data from device TD11');
             break;
           case 5:
-
+            console.log('ya tut !!!!!');
           //  console.log(dev);
-            var channel = dev.get_channel(1);
+          //  var channel = dev.get_channel(1);
             //let validChannel = channel!==undefined&&channel.num_channel!==undefined&&channel.name!==undefined;
             // if(validChannel)
             // {
