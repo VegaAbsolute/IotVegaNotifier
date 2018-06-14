5ae35909ea114133e1912d743964bf55558e984c
Gerusenko <a.gerusenko@vega-absolute.ru>
2018-06-14 09:01:20 +0700

    test lifeTime

--- libs/vega_linphone.js
+++ libs/vega_linphone.js
@@ -431,7 +431,8 @@ class VegaLinphone extends RHvoice
     let timePassed = firstTime?(currentTime-firstTime):0;
     console.log('timePassed = ',timePassed);
     delete this._stack[uuid];
-    let lifeTime = timePassed<86400000;
+    // let lifeTime = timePassed<86400000;
+    let lifeTime = timePassed<120000;
     if(type == 'voicemessage' && lifeTime)
     {
       this.pushVoiceMessage(mess,tel,firstTime);
