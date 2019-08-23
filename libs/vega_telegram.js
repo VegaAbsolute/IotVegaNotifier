a85b9991ef0138ee1f561caff087fdc28e7ed9e0
Gerusenko <a.gerusenko@vega-absolute.ru>
2019-08-23 09:59:15 +0700

    Удалил муср

--- libs/vega_telegram.js
+++ libs/vega_telegram.js
@@ -78,15 +78,6 @@ class VegaTelegram extends EventEmitter
     this._connect._timeLastUpdate = new Date().getTime();
     this._connect.on('polling_error',this._error);
     this._connect.onText(/\/chatid/,this._getChatID);
-    bot.onText(/\/id/, (msg) => {
-      // 'msg' is the received Message from Telegram
-    
-      var chatId = msg.chat.id;
-      var resp = 'test'; // the captured "whatever"
-    
-      // send back the matched "whatever" to the chat
-      bot.sendMessage(chatId, resp);
-    });
     this._connect._self = this;
   }
   _getChatID(msg)
